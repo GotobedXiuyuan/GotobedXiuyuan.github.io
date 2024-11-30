f163040283f0593894d49b838775d1fc4b7f26df
GotobedXiuyuan <78220045+GotobedXiuyuan@users.noreply.github.com>
2024-11-29 21:53:34 -0500

    update

---
+++ assets/vendor/remixicon/A.java
@@ -0,0 +1,38 @@
+<section id="skills" class="services">
+    <div class="container">
+        <div class="section-title">
+            <h2>Skills</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-12" style="display:block;">
+
+                <!-- Programming Languages -->
+                <div class="col-md-12 mt-4 mt-md-0 icon-box" style="background:#fff; border:1px solid #ddd; margin-bottom: 15px;">
+                    <h4 style="text-align:left;color:#09203a">Programming Languages</h4>
+                    <p style="text-align:left; margin: 0; padding: 10px;">
+                        C, C++, Python, Java, JavaScript, HTML, CSS, Solidity, SQL, R
+                    </p>
+                </div>
+
+                <!-- Frameworks and Technologies -->
+                <div class="col-md-12 mt-4 mt-md-0 icon-box" style="background:#fff; border:1px solid #ddd; margin-bottom: 15px;">
+                    <h4 style="text-align:left;color:#09203a">Frameworks and Technologies</h4>
+                    <p style="text-align:left; margin: 0; padding: 10px;">
+                        <strong>Frontend:</strong> Vue, React <br>
+                        <strong>Backend:</strong> Spring Boot, Django, Node.js, Express.js <br>
+                        <strong>Libraries/Tools:</strong> Numpy, Matplotlib, TensorFlow, PyTorch <br>
+                        <strong>Databases:</strong> MongoDB, MySQL
+                    </p>
+                </div>
+
+                <!-- Tools -->
+                <div class="col-md-12 mt-4 mt-md-0 icon-box" style="background:#fff; border:1px solid #ddd; margin-bottom: 15px;">
+                    <h4 style="text-align:left;color:#09203a">Tools</h4>
+                    <p style="text-align:left; margin: 0; padding: 10px;">
+                        Amazon AWS, Apache Hadoop, Spark, Google Colab, Docker, Jenkins CI/CD, JUnit, Playwright
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
